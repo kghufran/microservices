089e76006c4082c12fc612e24f0e2a46cc77105a
Ghufran Ahmad Khan <kghufran81@gmail.com>
2020-01-04 22:23:24 +0300

    added controller and config class

---
+++ limits-service/src/main/java/com/ghufran/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,31 @@
+package com.ghufran.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimum;
+	
+	protected LimitConfiguration() {
+		
+	}
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	
+}
