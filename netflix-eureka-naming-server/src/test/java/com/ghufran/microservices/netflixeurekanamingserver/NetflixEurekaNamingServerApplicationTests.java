d7b6e2eb3b2522477c8966a6586ae0cebf8c6ee5
Ghufran Ahmad Khan <kghufran81@gmail.com>
2020-01-05 23:53:55 +0300

    added eureka server

---
+++ netflix-eureka-naming-server/src/test/java/com/ghufran/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghufran.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
