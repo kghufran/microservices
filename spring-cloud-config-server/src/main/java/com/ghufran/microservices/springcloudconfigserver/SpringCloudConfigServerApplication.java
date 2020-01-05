b58cdf5e4047e2b6ed89e6231fe9903f42b44881
Ghufran Ahmad Khan <kghufran81@gmail.com>
2020-01-05 15:02:56 +0300

    config server

--- spring-cloud-config-server/src/main/java/com/ghufran/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/ghufran/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,9 +2,7 @@ package com.ghufran.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
