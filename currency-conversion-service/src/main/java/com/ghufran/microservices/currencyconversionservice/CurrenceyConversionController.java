5d5171d33afa8c50189c5ce405e6d4470bab2e25
Ghufran Ahmad Khan <kghufran81@gmail.com>
2020-01-05 23:11:50 +0300

    initial commit

---
+++ currency-conversion-service/src/main/java/com/ghufran/microservices/currencyconversionservice/CurrenceyConversionController.java
@@ -0,0 +1,60 @@
+package com.ghufran.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrenceyConversionController {
+
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/amount/{amount}")
+	public CurrencyConversionBean convertCurrency(@PathVariable String from,
+			@PathVariable String to, @PathVariable BigDecimal amount) {
+		
+		Map<String, String> uriValriables = new HashMap<String, String>();
+		uriValriables.put("from", from);
+		uriValriables.put("to", to);
+		
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversionBean.class, 
+				uriValriables);
+		CurrencyConversionBean response = responseEntity.getBody();
+		
+		return new CurrencyConversionBean(
+				response.getId(),
+				from, 
+				to, 
+				response.getConversionFactor(), 
+				amount, 
+				amount.multiply(response.getConversionFactor()), 
+				response.getPort());
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/amount/{amount}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+			@PathVariable String to, @PathVariable BigDecimal amount) {
+		
+		CurrencyConversionBean response = proxy.retreiveExchangeValue(from, to);
+		
+		return new CurrencyConversionBean(
+				response.getId(),
+				from, 
+				to, 
+				response.getConversionFactor(), 
+				amount, 
+				amount.multiply(response.getConversionFactor()), 
+				response.getPort());
+	}
+	
+}
---
+++ currency-conversion-service/src/main/java/com/ghufran/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,72 @@
+package com.ghufran.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal amount;
+	private BigDecimal calculatedAmount;
+	private int port;
+	
+	public CurrencyConversionBean() {
+		
+	}
+	
+	public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal amount,
+			BigDecimal calculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.amount = amount;
+		this.calculatedAmount = calculatedAmount;
+		this.port = port;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+	public BigDecimal getCalculatedAmount() {
+		return calculatedAmount;
+	}
+	public void setCalculatedAmount(BigDecimal calculatedAmount) {
+		this.calculatedAmount = calculatedAmount;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+}
